d7139205c4c717f05651e9b8612d4c31f8dff951
gprando <gprando55@gmail.com>
2022-01-30 17:55:13 -0300

    chore: update exercise

--- coursera-bootcamp/Getting-Started-with-Go/Module-3-Activity/slice.go
+++ coursera-bootcamp/Getting-Started-with-Go/Module-3-Activity/slice.go
@@ -1,19 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	sliceNumbers := []int{34, 22, 1}
+	sliceNumbers := make([]int, 3)
 
-	sortSlice(sliceNumbers)
-	fmt.Println(sliceNumbers)
+	input := bufio.NewScanner(os.Stdin)
 
-	sliceNumbers = append(sliceNumbers, 16)
-	sortSlice(sliceNumbers)
-	fmt.Println(sliceNumbers)
+	fmt.Print("Enter number OR press X for close the program :D ")
+	var i int
+	var inputEntry string
+
+	for input.Scan() {
+		inputEntry = input.Text()
+		inputInt, err := strconv.Atoi(inputEntry)
+
+		if strings.EqualFold(inputEntry, "X") {
+			break
+		}
+
+		if err != nil {
+			fmt.Println("Input does not number and X, error ->", err.Error())
+			break
+		}
+
+		sliceNumbers[i] = inputInt
+		i++
+
+		if len(sliceNumbers) == i {
+			sliceNumbers = append(sliceNumbers, i)
+		}
+
+		sortSlice(sliceNumbers)
+		fmt.Println("sortred ", sliceNumbers)
+
+		fmt.Println("Enter new number OR press X to break program.")
+
+	}
+
 }
 
 func sortSlice(sliceNumbers []int) {
